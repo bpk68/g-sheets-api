a816878c8002901884ea9fc169932c6b056ea1e1
Rob Kendal <38687284+bpk68@users.noreply.github.com>
2021-08-24 07:03:14 +0100

    Adds support for special characters in sheet names

--- src/gsheetsapi.js
+++ src/gsheetsapi.js
@@ -2,7 +2,7 @@ import fetch from 'cross-fetch';
 
 const gsheetsAPI = function ({apiKey, sheetId, sheetName, sheetNumber = 1}) {
   try {
-    const sheetNameStr = sheetName || `Sheet${sheetNumber}`
+    const sheetNameStr = encodeURIComponent(sheetName) || `Sheet${sheetNumber}`
     const sheetsUrl = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${sheetNameStr}?dateTimeRenderOption=FORMATTED_STRING&majorDimension=ROWS&valueRenderOption=FORMATTED_VALUE&key=${apiKey}`;
 
     return fetch(sheetsUrl)
